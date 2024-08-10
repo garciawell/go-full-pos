d38db7715a875903475903eb40482af46fa9840e
Wellington Garcia <garciawell@gmail.com>
2024-08-09 23:28:33 -0300

    feat: add logs

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -13,28 +14,23 @@ func main() {
 	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*300)
 	defer cancel()
 
-	body := getCotacao(ctx)
-	createFile(body)
-
-}
-
-func getCotacao(ctx context.Context) []byte {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8080/cotacao", nil)
+	body, err := getCotacao(ctx)
 	if err != nil {
 		panic(err)
 	}
+	createFile(body)
+}
+
+func getCotacao(ctx context.Context) (body []byte, err error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8080/cotacao", nil)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
+	body, err = io.ReadAll(resp.Body)
 
-	return body
-
+	return body, err
 }
 
 func createFile(body []byte) {
@@ -43,6 +39,6 @@ func createFile(body []byte) {
 		panic(err)
 	}
 	defer f.Close()
-
+	fmt.Println("Arquivo criado com sucesso...")
 	f.Write(body)
 }
