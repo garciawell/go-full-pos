a7236b04a27ed3c36c0554dd23dd78e0582a064b
Wellington Garcia <garciawell@gmail.com>
2024-09-02 21:57:43 -0300

    update data db

--- 22-database/main.go
+++ 22-database/main.go
@@ -32,6 +32,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	product.Price = 100
+	err = updateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
@@ -44,5 +49,19 @@ func insertProduct(db *sql.DB, product *Product) error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("UPDATE products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
