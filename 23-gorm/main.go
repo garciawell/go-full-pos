6bcfb611112e3beb74e8f72908fd0176a7833ff0
Wellington Garcia <garciawell@gmail.com>
2024-09-04 22:28:17 -0300

    feat: select products

--- 23-gorm/main.go
+++ 23-gorm/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -20,16 +22,32 @@ func main() {
 	db.AutoMigrate(&ProductInfo{})
 
 	// create
-	db.Create(&ProductInfo{
-		Name:  "Iphone",
-		Price: 1000.50,
-	})
+	// db.Create(&ProductInfo{
+	// 	Name:  "Iphone",
+	// 	Price: 1000.50,
+	// })
 
-	// Create Batch
-	products := []ProductInfo{
-		{Name: "Notebook", Price: 120},
-		{Name: "Android", Price: 20},
-		{Name: "Keyboard", Price: 50},
+	// // Create Batch
+	// products := []ProductInfo{
+	// 	{Name: "Notebook", Price: 120},
+	// 	{Name: "Android", Price: 20},
+	// 	{Name: "Keyboard", Price: 50},
+	// }
+	// db.Create(&products)
+
+	// Select One
+
+	var product ProductInfo
+	// db.First(&product, 1)
+	// fmt.Println(product)
+
+	db.First(&product, "name = ?", "Iphone")
+	fmt.Println(product)
+
+	var products []ProductInfo
+	db.Find(&products)
+
+	for _, p := range products {
+		fmt.Println(p)
 	}
-	db.Create(&products)
 }
