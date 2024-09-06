35a75cbde7e8c33691ba43d571b432d84a68b3d4
Wellington Garcia <garciawell@gmail.com>
2024-09-06 00:32:17 -0300

    Mocks

--- 27-testfy/tax.go
+++ 27-testfy/tax.go
@@ -14,3 +14,25 @@ func CalculateTax(amount float64) (float64, error) {
 	}
 	return 5, nil
 }
+
+type Repository interface {
+	SaveTax(amount float64) error
+}
+
+func CalculateTaxAndSave(amount float64, repository Repository) error {
+	tax := CalculateTax2(amount)
+	return repository.SaveTax(tax)
+}
+
+func CalculateTax2(amount float64) float64 {
+	if amount <= 0 {
+		return 0.0
+	}
+	if amount >= 1000 && amount < 20000 {
+		return 10
+	}
+	if amount >= 20000 {
+		return 20.0
+	}
+	return 5
+}
--- 27-testfy/tax_test.go
+++ 27-testfy/tax_test.go
@@ -1,6 +1,7 @@
 package tax
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +15,18 @@ func TestCalculate(t *testing.T) {
 	tax, err = CalculateTax(0)
 	assert.Error(t, err, "Amount must be grather than 0")
 	assert.Equal(t, 0.0, tax)
+}
+
+func TestCalculateAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil)
+	repository.On("SaveTax", 0.0).Return(errors.New("Error Save"))
+	err := CalculateTaxAndSave(1000, repository)
+	assert.Nil(t, err)
+
+	err = CalculateTaxAndSave(0.0, repository)
+	assert.Error(t, err, "Error Save")
+
+	repository.AssertExpectations(t)
+
 }
