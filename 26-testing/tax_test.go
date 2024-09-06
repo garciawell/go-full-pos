d5a93599bdcc8ebdce7721574dc8e0c13ca8cdaa
Wellington Garcia <garciawell@gmail.com>
2024-09-05 23:31:14 -0300

    feat: loop test

--- 26-testing/tax_test.go
+++ 26-testing/tax_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestTax(t *testing.T) {
 	amount := 500.0
-	expected := 6.0
+	expected := 5.0
 
 	result := CalculateTax(amount)
 
@@ -12,4 +12,24 @@ func TestTax(t *testing.T) {
 		t.Errorf("Expected %f but got %f", expected, result)
 	}
 
+}
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expected float64
+	}
+
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+
+		if result != item.expected {
+			t.Errorf("Expected %f but got %f", item.expected, result)
+		}
+	}
 }
