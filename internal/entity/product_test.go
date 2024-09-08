dd45cac4f4b9d32424dd46b3e501ba67ee94028c
Wellington Garcia <garciawell@gmail.com>
2024-09-08 18:50:51 -0300

    test pagination

--- internal/entity/product_test.go
+++ internal/entity/product_test.go
@@ -7,16 +7,16 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.00)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
 	assert.Equal(t, p.Name, "Product 1")
-	assert.Equal(t, p.Price, 10)
+	assert.Equal(t, p.Price, 10.00)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	p, err := NewProduct("", 10)
+	p, err := NewProduct("", 10.00)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrNamesRequired, err)
 }
@@ -26,13 +26,13 @@ func TestProductWhenPriceIsRequired(t *testing.T) {
 	assert.Equal(t, ErrPriceRequired, err)
 }
 func TestProductWhenPriceInvalid(t *testing.T) {
-	p, err := NewProduct("Product 1", -10)
+	p, err := NewProduct("Product 1", -10.00)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrPriceIndalid, err)
 }
 
 func TestProductValidate(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.00)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.Nil(t, p.Validate())
