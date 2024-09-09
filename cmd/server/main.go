af9ad1e63462ceea90a754d22025d2d58c4ac6c7
Wellington Garcia <garciawell@gmail.com>
2024-09-09 20:54:33 -0300

    Chi router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,23 +7,32 @@ import (
 	"github.com/garciawell/go-full-pos/apis/internal/entity"
 	"github.com/garciawell/go-full-pos/apis/internal/infra/database"
 	"github.com/garciawell/go-full-pos/apis/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func main() {
+var productHandler *handlers.ProductHandler
+
+func init() {
 	_, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
+
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 	db.AutoMigrate(&entity.User{}, &entity.Product{})
 	productDB := database.NewProduct(db)
-	productHandler := handlers.NewProductHandler(productDB)
+	productHandler = handlers.NewProductHandler(productDB)
+}
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8000", nil)
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", r)
 }
