bcea0f2ef9b4d425c93761d0424049b9cf3d1c5c
Wellington Garcia <garciawell@gmail.com>
2024-09-14 15:37:54 -0300

    feat: send messages

---
+++ 29-eventos/cmd/consumer/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	rabbitmq "github.com/garciawell/go-full-pos/events/pkg/rabbitMQ"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	msgs := make(chan amqp.Delivery)
+
+	go rabbitmq.Consume(ch, msgs)
+
+	for msg := range msgs {
+		fmt.Println(string(msg.Body))
+		msg.Ack(false)
+	}
+}
