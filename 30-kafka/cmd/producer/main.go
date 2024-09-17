2d20146385b6282c29314fd4a70d8e808c3419c2
Wellington Garcia <garciawell@gmail.com>
2024-09-16 21:19:33 -0300

    featL publish messge kafka

--- 30-kafka/cmd/producer/main.go
+++ 30-kafka/cmd/producer/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Kafka Producer")
+	producer := NewKafkaProducer()
+	err := Publish("mensagem 1", "teste", producer, nil)
+	if err != nil {
+		log.Println("EPAA")
+	}
+	log.Println("Producer criado")
+	producer.Flush(1000)
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "30-kafka-kafka-1:9092",
+	}
+
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println("Error creating producer", err.Error())
+	}
+	return p
+}
+
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+
+	message := &kafka.Message{
+		Value:          []byte(msg),
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Key:            key,
+	}
+
+	err := producer.Produce(message, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- 30-kafka/go.mod
+++ 30-kafka/go.mod
@@ -1,3 +1,5 @@
 module github.com/garciawell/go-full-pos/kafka
 
 go 1.21.13
+
+require github.com/confluentinc/confluent-kafka-go v1.9.2
