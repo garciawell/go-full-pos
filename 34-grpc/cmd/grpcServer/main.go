ab1f64a798b3c2b6a3a4d959ceb60f2cce76f86e
Wellington Garcia <garciawell@gmail.com>
2024-10-30 23:11:44 -0300

    feat: call GRPC evans

--- 34-grpc/cmd/grpcServer/main.go
+++ 34-grpc/cmd/grpcServer/main.go
@@ -7,16 +7,19 @@ import (
 	"github.com/garciawell/go-full-pos/34-grpc/internal/database"
 	"github.com/garciawell/go-full-pos/34-grpc/internal/pb"
 	"github.com/garciawell/go-full-pos/34-grpc/internal/service"
+	_ "github.com/mattn/go-sqlite3"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	db, err := sql.Open("sqlite", "./db.sqlite")
+	db, err := sql.Open("sqlite3", "./db.sqlite")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+
+	println("Connected to database...")
 
 	categoryDB := database.NewCategory(db)
 	categoryService := service.NewCategoryService(*categoryDB)
