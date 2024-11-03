a0443241975638c0fb45e8b9f285195f7c2d6659
Wellington Garcia <garciawell@gmail.com>
2024-11-03 18:16:24 -0300

    feat: upload go to aws s3

---
+++ 35-upload/cmd/uploader/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	s3Client *s3.S3
+	s3Bucket string
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	awsPublic := os.Getenv("AWS_PUBLIC_KEY")
+	awsSecret := os.Getenv("AWS_SECRET_KEY")
+
+	session, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+		Credentials: credentials.NewStaticCredentials(
+			awsPublic,
+			awsSecret,
+			"",
+		),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	s3Client = s3.New(session)
+	s3Bucket = "font-gg"
+}
+
+func main() {
+	dir, err := os.Open("./tmp")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+
+	for {
+		files, err := dir.ReadDir(1)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		uploadFile(files[0].Name())
+	}
+
+}
+
+func uploadFile(filename string) {
+	completeFileName := fmt.Sprintf("./tmp/%s", filename)
+	fmt.Printf("Uploading file %s to bucket %s\n", completeFileName, s3Bucket)
+	file, err := os.Open(completeFileName)
+	if err != nil {
+		fmt.Printf("Failed to open file %s, %v\n", completeFileName, err)
+		return
+	}
+	defer file.Close()
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(filename),
+		Body:   file,
+	})
+	if err != nil {
+		fmt.Printf("Failed to upload file %s, %v\n", completeFileName, err)
+		return
+	}
+	fmt.Printf("File %s uploaded successfully\n", completeFileName)
+}
--- 35-upload/go.mod
+++ 35-upload/go.mod
@@ -4,4 +4,7 @@ go 1.21.0
 
 require github.com/aws/aws-sdk-go v1.55.5
 
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
