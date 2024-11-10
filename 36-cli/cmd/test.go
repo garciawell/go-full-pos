24563a986af14fa660710fa533ca656478130c40
Wellington Garcia <garciawell@gmail.com>
2024-11-10 15:05:48 -0300

    feat: cobra test

--- 36-cli/cmd/test.go
+++ 36-cli/cmd/test.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,9 +9,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// pingCmd represents the ping command
-var pingCmd = &cobra.Command{
-	Use:   "ping",
+// testCmd represents the test command
+var testCmd = &cobra.Command{
+	Use:   "test",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,20 +20,30 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ping called")
+		fmt.Println("test called")
+		command, _ := cmd.Flags().GetString("--command")
+		if command == "ping" {
+			fmt.Println("pong")
+		} else if command == "pong" {
+			fmt.Println("ping")
+		} else {
+			fmt.Println("Comando não reconhecido")
+		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(pingCmd)
+	rootCmd.AddCommand(testCmd)
+	testCmd.Flags().StringP("--command", "c", "", "Escolha Ping ou Pong")
+	testCmd.MarkFlagRequired("--command")
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// pingCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// testCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// pingCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// testCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
