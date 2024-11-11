83bc60809c7177035baf7f1e298e049b62c6b31b
Wellington Garcia <garciawell@gmail.com>
2024-11-11 19:29:07 -0300

    feat: cobra cli

--- 36-cli/cmd/category.go
+++ 36-cli/cmd/category.go
@@ -24,7 +24,7 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(categoryCmd)
-
+	categoryCmd.PersistentFlags().StringP("name", "n", "", "Name of the category")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
