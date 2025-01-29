5f37157382a0ecb363dab90c699f64eab0328194
Wellington Garcia <garciawell@gmail.com>
2025-01-28 21:13:12 -0300

    feat: config mondo conn

--- 01-challenge-auction/cmd/auction/main.go
+++ 01-challenge-auction/cmd/auction/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"context"
 	"log"
 
+	"github.com/garciawell/go-challenge-auction/config/database/mongodb"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
+	ctx := context.Background()
+
+	if err := godotenv.Load("cmd/auction/.env"); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
+		return
+	}
+
+	_, err := mongodb.NewMongoDBConn(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
 		return
 	}
 }
